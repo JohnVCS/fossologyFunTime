0d0f25d9a9f7e3b8af4935e49ba149604f0ae94d
jmoseman01 <jmoseman01@gmail.com>
2016-02-02 00:03:07 -0600

    merged john's insert function with nomos parsing

--- happy.py
+++ happy.py
@@ -1,4 +1,29 @@
 import subprocess
+import MySQLdb
+
+def insertFilenameLicenses(filename,licenses):
+	# Open database connection
+	db = MySQLdb.connect("localhost","root","jm98741236","fossologyDB")
+
+	# prepare a cursor object using cursor() method
+	cursor = db.cursor()
+
+	# prepare SQL query to INSERT a record into the database.
+	sql = "INSERT INTO FILE_LICENSES(FileName, Licenses) \
+	       VALUES ('%s', '%s')" % \
+	       (filename, licenses)
+
+	try:
+	   # Execute the SQL command
+	   cursor.execute(sql)
+	   # Commit your changes in the database
+	   db.commit()
+	except:
+	   # Rollback in case there is any error
+	   db.rollback()
+
+	# disconnect from server
+	db.close()
 
 nomosProcess=subprocess.Popen(["/usr/share/fossology/nomos/agent/nomos" ,"-d","openssl-1.0.2e/"], stdout=subprocess.PIPE)
 deleteFirst4LinesProcess=subprocess.Popen(["sed" ,"1,4d"], stdin=nomosProcess.stdout,stdout=subprocess.PIPE)
@@ -13,5 +38,6 @@ for line in end_of_pipe:
 		licenses=",".join(splitline[4:])
 		print "filename", filename
 		print "licenses", licenses 
+		insertFilenameLicenses(filename,licenses)
 	except IndexError:
 		print "index error", splitline
