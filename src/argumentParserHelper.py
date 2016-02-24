0628ffbbb52df453c429d79381adbc880da14737
JohnVCS <jbviernesiv@gmail.com>
2016-02-23 19:31:50 -0600

    Update argumentParserHelper.py
    Added license (copyright)

--- src/argumentParserHelper.py
+++ src/argumentParserHelper.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python
+
+# Copyright (C) 2015 Jesse Moseman, and John Carlo B. Viernes IV
+#
+# This file is part of fossologyFunTime.
+#
+# fossologyFunTime is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# fossologyFunTime is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with fossologyFunTime.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import argparse
 import configParserHelper
 #./happy.py -d directoryPackage -u name -p password
