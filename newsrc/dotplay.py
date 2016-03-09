c13e55532e7277b0a5abd7072ac19447e5d5eb39
jmoseman01 <jmoseman01@gmail.com>
2016-03-09 17:44:08 -0600

    grab successors

--- newsrc/dotplay.py
+++ newsrc/dotplay.py
@@ -1,4 +1,8 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 G=nx.drawing.nx_agraph.read_dot("depmetadata.dot")
-print G.nodes()
+#print G.nodes()
+#print G.successors(1)
+for n in G.nodes_iter():
+	print (n,G.successors(n))
+
