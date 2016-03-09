89d497ebdae900a0f7d65b891618397e7e84f5a4
jmoseman01 <jmoseman01@gmail.com>
2016-03-09 14:13:41 -0600

    makes directory

--- newsrc/main.py
+++ newsrc/main.py
@@ -1,6 +1,11 @@
 import subprocess
+import os
+ 
+newpath = r'mydep' 
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
 
-copyDepCmd=["mvn","dependency:copy-dependencies","-DoutputDirectory=/tmp/mydep","-Dclassifier=sources"]
+copyDepCmd=["mvn","dependency:copy-dependencies","-DoutputDirectory=mydep","-Dclassifier=sources"]
 copyDepMvnPluginProcess=subprocess.Popen(copyDepCmd, stdout=None)
 #deleteFirst4LinesProcess=subprocess.Popen(["sed" ,"1,4d"], stdin=nomosProcess.stdout,stdout=subprocess.PIPE)
 #end_of_pipe=deleteFirst4LinesProcess.stdout
