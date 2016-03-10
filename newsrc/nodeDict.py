8a8a910697aecbd4049a33e1be4cf21ea158c448
jmoseman01 <jmoseman01@gmail.com>
2016-03-09 20:41:55 -0600

    as list

--- newsrc/nodeDict.py
+++ newsrc/nodeDict.py
@@ -2,6 +2,8 @@ import networkx
 
 graph=networkx.read_graphml("test.graphml")
 nodesDict=dict(graph.nodes(data="NodeLabel"))
+edgeLabels=[]
 for e1,e2 in graph.edges():
-	print (nodesDict[e1]['label'],nodesDict[e2]['label'])
+	edgeLabels.append((nodesDict[e1]['label'],nodesDict[e2]['label']))
 
+print edgeLabels
