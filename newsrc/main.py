9ca660e451ed5355097a06405c95dfa4ddde3052
jmoseman01 <jmoseman01@gmail.com>
2016-03-12 20:36:48 -0600

    get rid of hang

--- newsrc/main.py
+++ newsrc/main.py
@@ -2,6 +2,8 @@ import subprocess
 import os
 
  
+devnull=open('/dev/null','w')
+
 # creates the temporary directory to store the jar files
 def createTempDirectory():
 	newpath = r'mydep' 
@@ -11,13 +13,13 @@ def createTempDirectory():
 # copies depedencies to folder 
 def copyDependencyToTempFolder():
 	copyDepCmd=["mvn","dependency:copy-dependencies","-DoutputDirectory=mydep","-Dclassifier=sources"]
-	copyDepMvnPluginProcess=subprocess.Popen(copyDepCmd, stdout=None)
+	copyDepMvnPluginProcess=subprocess.Popen(copyDepCmd)
 
 # creates the graphML
 # graphML is one of the supported output type format of Maven
 def createGraphMl():
 	createGraphMlCommand = ["mvn", "dependency:tree","-DoutputFile=test.graphml", "-DoutputType=graphml"]
-	createGraphMlMvnPluginProcess = subprocess.Popen(createGraphMlCommand, stdout = None)
+	createGraphMlMvnPluginProcess = subprocess.Popen(createGraphMlCommand, stdout=devnull)
 
 #deleteFirst4LinesProcess=subprocess.Popen(["sed" ,"1,4d"], stdin=nomosProcess.stdout,stdout=subprocess.PIPE)
 #end_of_pipe=deleteFirst4LinesProcess.stdout
