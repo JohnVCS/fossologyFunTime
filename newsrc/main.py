994be749aa29db7882aa4a851abf2c2a752e073b
JohnVCS <jbviernesiv@gmail.com>
2016-03-12 20:29:33 -0600

    edited main.py, but for some reason it's not giving the correct output

--- newsrc/main.py
+++ newsrc/main.py
@@ -1,17 +1,28 @@
 import subprocess
 import os
+
  
-#creates directory
-newpath = r'mydep' 
-if not os.path.exists(newpath):
-    os.makedirs(newpath)
+# creates the temporary directory to store the jar files
+def createTempDirectory():
+	newpath = r'mydep' 
+	if not os.path.exists(newpath):
+    		os.makedirs(newpath)
 
-#copies depedencies to folder 
-copyDepCmd=["mvn","dependency:copy-dependencies","-DoutputDirectory=mydep","-Dclassifier=sources"]
-copyDepMvnPluginProcess=subprocess.Popen(copyDepCmd, stdout=None)
+# copies depedencies to folder 
+def copyDependencyToTempFolder():
+	copyDepCmd=["mvn","dependency:copy-dependencies","-DoutputDirectory=mydep","-Dclassifier=sources"]
+	copyDepMvnPluginProcess=subprocess.Popen(copyDepCmd, stdout=None)
 
+# creates the graphML
+# graphML is one of the supported output type format of Maven
+def createGraphMl():
+	createGraphMlCommand = ["mvn", "dependency:tree","-DoutputFile=test.graphml", "-DoutputType=graphml"]
+	createGraphMlMvnPluginProcess = subprocess.Popen(createGraphMlCommand, stdout = None)
 
 #deleteFirst4LinesProcess=subprocess.Popen(["sed" ,"1,4d"], stdin=nomosProcess.stdout,stdout=subprocess.PIPE)
 #end_of_pipe=deleteFirst4LinesProcess.stdout
 #return end_of_pipe 
+
+if __name__ == '__main__':
+	createGraphMl()
 
